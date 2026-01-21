316464439a5acb3d5defd1c4a80291cc7c9c4027
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-01-21 22:54:23 +0530

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer style={{ textAlign: 'center', padding: '10px', borderTop: '1px solid #ccc', marginTop: '20px' }}>
+            <p>&copy; 2024 MyCompany. All rights reserved.</p>
+        </footer>
+    </>
+  )
+}
+
+export default Footer;
